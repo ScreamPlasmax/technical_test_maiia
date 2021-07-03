d2269292c606df4369a5718f1bb0cd4e777c56dd
Maxime Campane <mcampane@steamulo.com>
2021-07-03 17:33:11 +0200

    Strongly typed the store and added a new customHook already typed "useAppDispatch"

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import reducers from './reducers';
+import { useDispatch } from 'react-redux';
+
+export type StoreType = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers(reducers);
 
-export default configureStore({
+const store = configureStore<StoreType>({
   reducer: rootReducer,
   devTools: true,
 });
+
+export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
+
+export default store;
