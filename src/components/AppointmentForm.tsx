058f07cab4f5ccdf17df3538c6fe817a278b811a
Maxime Campane <mcampane@steamulo.com>
2021-07-04 15:36:17 +0200

    Fetched practitioners and displayed them with their specialty in the select input, with some style added

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -2,6 +2,11 @@ import { Form, Formik } from 'formik';
 import Button from 'components/atoms/buttons/Button';
 import { makeStyles, MenuItem } from '@material-ui/core';
 import SelectField from 'components/atoms/selects/SelectField';
+import { useEffect } from 'react';
+import { StoreType, useAppDispatch } from 'store';
+import { useSelector } from 'react-redux';
+import { getPractitioners, practitionersSelectors } from 'store/practitioners';
+import { Practitioner } from '@prisma/client';
 
 const initialValues = {
   practitioner: null,
@@ -26,10 +31,25 @@ const useStyles = makeStyles((theme) => ({
   formContent: {
     padding: '24px',
   },
+  menuItemRoot: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  practitionerSpecialty: {
+    color: '#3FB0AA',
+  },
 }));
 
 const AppointmentForm = () => {
   const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const practitioners = useSelector<StoreType, Practitioner[]>((state) =>
+    practitionersSelectors.selectAll(state.practitioners),
+  );
+
+  useEffect(() => {
+    dispatch(getPractitioners());
+  }, []);
 
   return (
     <div className={classes.form}>
@@ -45,9 +65,14 @@ const AppointmentForm = () => {
       >
         <Form className={classes.formContent}>
           <SelectField name="practitioner" label="Pratitien" disabledLabel="Choisissez un praticien">
-            <MenuItem value="1">Gérard</MenuItem>
-            <MenuItem value="2">Emilie</MenuItem>
-            <MenuItem value="3">Paul</MenuItem>
+            {practitioners.map((practitioner) => (
+              <MenuItem key={practitioner.id} value={practitioner.id} classes={{ root: classes.menuItemRoot }}>
+                <span>
+                  {practitioner.firstName} {practitioner.lastName}
+                </span>
+                <span className={classes.practitionerSpecialty}>{practitioner.speciality}</span>
+              </MenuItem>
+            ))}
           </SelectField>
           <SelectField name="patient" label="Patient" disabledLabel="Choisissez un patient">
             <MenuItem value="1">Option 1</MenuItem>
