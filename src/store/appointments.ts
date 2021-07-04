1ddb8cfcfbc45ecb3a2b5b37719039bde1e751a2
Maxime Campane <mcampane@steamulo.com>
2021-07-04 22:04:29 +0200

    Added appointments reducer

---
+++ src/store/appointments.ts
@@ -0,0 +1,47 @@
+import { Appointment } from '@prisma/client';
+import { createAsyncThunk, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { parseIds } from 'store/utils';
+import { SERVER_API_ENDPOINT } from 'store/timeslots';
+
+export const getAppointments = createAsyncThunk('getAppointments', async () => {
+  const response = await fetch(`${SERVER_API_ENDPOINT}/appointments`);
+  const parsedResponse = await response.json();
+  return parseIds(parsedResponse) as Appointment[];
+});
+
+export const postAppointment = createAsyncThunk('postAppointment', async (body: string) => {
+  const response = await fetch(`${SERVER_API_ENDPOINT}/appointments`, { method: 'POST', body: body });
+  const parsedResponse = await response.json();
+  return parseIds(parsedResponse) as Appointment;
+});
+
+const appointmentsAdapter = createEntityAdapter<Appointment>({
+  sortComparer: (a, b) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime(),
+});
+
+export const appointmentsSelectors = appointmentsAdapter.getSelectors();
+
+const appointmentsSlice = createSlice({
+  name: 'timeslots',
+  initialState: appointmentsAdapter.getInitialState({
+    loading: false,
+    error: null,
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAppointments.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getAppointments.fulfilled, (state, action) => {
+      appointmentsAdapter.setAll(state, action.payload);
+      state.error = null;
+      state.loading = false;
+    });
+    builder.addCase(getAppointments.rejected, (state, action) => {
+      state.error = action.error;
+      state.loading = false;
+    });
+  },
+});
+
+export default appointmentsSlice;
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,9 +1,11 @@
 import timeslots from './timeslots';
 import practitioners from 'store/practitioners';
 import patients from 'store/patients';
+import appointmentsSlice from 'store/appointments';
 
 export default {
   timeslots: timeslots.reducer,
   practitioners: practitioners.reducer,
   patients: patients.reducer,
+  appointments: appointmentsSlice.reducer,
 };
