a9435378f720fa8fbfb9f8cf489eddcb1d654316
Maxime Campane <mcampane@steamulo.com>
2021-07-04 23:37:32 +0200

    Added CardHeader component

---
+++ src/components/atoms/cards/CardHeader.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.secondary.light,
+    padding: '12px 16px 10px 16px',
+    color: theme.palette.common.white,
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+const CardHeader: FC = (props) => {
+  const { children } = props;
+  const classes = useStyle();
+
+  return <div className={classes.root}>{children}</div>;
+};
+
+export default CardHeader;
